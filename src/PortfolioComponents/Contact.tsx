1a21ee0c3f5b5dea9080c460fc7633a04024ab5b
Tanya627 <tanyaocampo96@yahoo.com>
2024-01-03 15:42:49 -0600

    created contact me section

--- src/PortfolioComponents/Contact.tsx
+++ src/PortfolioComponents/Contact.tsx
@@ -3,7 +3,30 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <div className="contact-me">
+        <div>
+          <img src="./person-logo.png" className="person-logo" />
+          <p className="my-name">Tanya Ocampo</p>
+        </div>
+
+        <h1>Contact Me</h1>
+        <div className="contact-info-container">
+          <div className="info-container">
+            <span className="material-symbols-outlined">location_on</span>
+            <p className="location">Chicago Metropolitan Area</p>
+          </div>
+          <div className="info-container">
+            <span className="material-symbols-outlined">phone_iphone</span>
+            <p className="phone-number">Upon Linked In Request</p>
+          </div>
+          <div className="info-container">
+            <span className="material-symbols-outlined">mail</span>
+            <a href="https://www.linkedin.com/in/tanyaocampo627/">
+              <p className="email">Send LinkedIn Mail</p>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
