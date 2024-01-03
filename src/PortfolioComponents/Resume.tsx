cc7b422975177e4e386e9a80f7e0971bb0f849c7
Tanya627 <tanyaocampo96@yahoo.com>
2024-01-03 16:22:05 -0600

    created download for resume, worked on resume component

--- src/PortfolioComponents/Resume.tsx
+++ src/PortfolioComponents/Resume.tsx
@@ -3,7 +3,27 @@ import "./Resume.css";
 const Resume = () => {
   return (
     <div>
-      <h1>RESUME</h1>
+      <div className="download-resume-container">
+        <p className="thanks-paragraph">
+          Thank you for taking the time to explore my portfolio and learn about
+          my professional journey. I appreciate your interest in my work and
+          experience. If you'd like to keep a summary of my skills, feel free to
+          download a copy of my resume. Your support and any opportunities for
+          collaboration or feedback are highly valued.
+        </p>
+        <p className="download-me-text">Download a copy of my resume:</p>
+        <div>
+          <a
+            href="./Ocampo_SWE_2024_Public_Resume.pdf"
+            download="TanyaOcampo_2024_Resume.pdf"
+          >
+            <button className="download-button">
+              <span className="material-symbols-outlined">download</span>CLICK
+              HERE
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
